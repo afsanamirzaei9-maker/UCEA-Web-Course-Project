2dd4dbc4d2412dc9f29c2900caad6f18441c1be0
Afsana <afsanamirzaei9@gmail.com>
2026-05-21 00:28:07 +0430

    main branch pulled again

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
+import { NavLink } from 'react-router-dom';
+import './navbar.css';
 export const Navbar = () => {
     return ( 
         <>
-            <div>
-                <ul>
-                    <h2>navbar</h2>
+            <div className="navbar-container border border-white w-screen ">
+                <div className="navLogo"><span>پروژه</span></div>
+                <ul className="navbar-ul">
+                    <li className="border border-blue-200"><NavLink to="/" className='border border-pink-400' >Home</NavLink></li>
+                    <li className="border border-blue-200"><NavLink to="/about" className='border border-pink-400' >About</NavLink></li>
+                    <li className="border border-blue-200"><NavLink to="/blog" className='border border-pink-400' >Blog</NavLink></li>
+                    <li className="border border-blue-200"><NavLink to="/panel" className='border border-pink-400' >Panel</NavLink></li>
+                    <li className="border border-blue-200"><NavLink to="/login" className='border border-pink-400' >Login</NavLink></li>
                 </ul>
             </div>    
         </>
