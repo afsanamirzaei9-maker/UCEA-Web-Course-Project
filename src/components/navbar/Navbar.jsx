7af92daf235989e7253f66d97a86cdc07d9d7e6e
Afsana <afsanamirzaei9@gmail.com>
2026-05-21 01:45:16 +0430

    local main replaced with remote maine

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import './navbar.css';
 export const Navbar = () => {
     return ( 
         <>
-            <div className="navbar-container border border-white w-screen bg-green-800">
+            <div className="navbar-container border border-white w-screen bg-red-800">
                 <div className="navLogo"><span>پروژه</span></div>
                 <ul className="navbar-ul">
-                    <li className="border border-blue-200"><NavLink to="/" className='border border-pink-400' >Home</NavLink></li>
+                    <li className="border border-blue-200 bg-green-800"><NavLink to="/" className='border border-pink-400' >Home</NavLink></li>
                     <li className="border border-blue-200"><NavLink to="/about" className='border border-pink-400' >About</NavLink></li>
                     <li className="border border-blue-200"><NavLink to="/blog" className='border border-pink-400' >Blog</NavLink></li>
                     <li className="border border-blue-200"><NavLink to="/panel" className='border border-pink-400' >Panel</NavLink></li>
