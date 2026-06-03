07a5927ab2851f87f6814dcb62f36aafe3455f0c
Afsana <afsanamirzaei9@gmail.com>
2026-06-03 07:57:23 +0430

    navbar made daynamic without responsive

---
+++ src/components/navbar/NavLinks.jsx
@@ -0,0 +1,10 @@
+
+import { NavLink } from 'react-router-dom'
+
+export default function NavLinks({title, path}) {
+  return (
+    <>
+        <li className="px-3 py-1"><NavLink to={path}className='text-decoration-none text-black'>{title}</NavLink></li>
+    </>
+  )
+}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
+import NavLinks from './NavLinks';
+import { navbarData } from './navbarData';
 export const Navbar = () => {
     return ( 
         <>
-            <div className="navbar-container border border-white w-full flex justify-between px-10 py-5 bg-purple-500">
+            <div className="navbar-container w-full flex justify-between px-10 py-3 bg-purple-500">
                 <div className="navLogo"><span>پروژه</span></div>
                 <ul className="navbar-ul flex">
-                    <li className="border border-blue-200 bg-green-800 p-3"><NavLink to="/" className='border border-pink-400 decoration-none' >Home</NavLink></li>
-                    <li className="border border-blue-200"><NavLink to="/about" className='border border-pink-400' >About</NavLink></li>
-                    <li className="border border-blue-200"><NavLink to="/blog" className='border border-pink-400' >Blog</NavLink></li>
-                    <li className="border border-blue-200"><NavLink to="/panel" className='border border-pink-400' >Panel</NavLink></li>
-                    <li className="border border-blue-200"><NavLink to="/login" className='border border-pink-400' >Login</NavLink></li>
+                    {navbarData.map((navLink)=>(
+                        <NavLinks 
+                        key={navLink.id}
+                        {...navLink}/>
+                    ))}
                 </ul>
                 {/* <button className="text-success"> read more</button> */}
             </div>    
