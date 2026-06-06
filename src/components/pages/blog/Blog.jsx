00df550f649ce6052c4059249db1b33c21a5dcc1
Afsana <afsanamirzaei9@gmail.com>
2026-06-07 03:52:11 +0430

    blog page completed successfully.

--- src/components/pages/blog/Blog.jsx
+++ src/components/pages/blog/Blog.jsx
@@ -1,7 +1,15 @@
+import { GreenHeading } from "../../heading/GreenHeading";
+import { Paragraph } from "../../text/Paragraph";
+import Articles from "./Articles";
+
 export const Blog = () => {
     return ( 
         <>
-            <h1 className="bg-blue-700">Blog Page</h1>
+           <div className="text-center m-5">
+                <GreenHeading text="به وبلاگ UCA خوش آمدید!" /> 
+                <Paragraph paragraph=" در اینجا جدید ترین مقالات علمی حوزه برنامه نویسی و تجربیات علمی را با شما به اشتراک می گذاریم. "/>
+           </div>  
+            <Articles/>
         </>
     );
 }
