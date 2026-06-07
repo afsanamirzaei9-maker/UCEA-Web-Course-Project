5354a1b84747b9f5043b39809e1ae65eb8ee6617
Mirzaei <afsanamirzaei9@gmail.com>
2026-06-07 06:31:33 +0430

    Add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/UCEA-Web-Course-Project/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
